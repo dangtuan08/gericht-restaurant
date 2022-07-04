e8682e271845908c16a48677ad8103d415eb66e3
dangtuan08 <48054696+dangtuan08@users.noreply.github.com>
2022-07-05 00:34:52 +0700

    AboutUs

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React from "react";
+import SubHeading from "../../components/SubHeading/SubHeading";
+import images from "../../constants/images";
 
-import './Header.css';
+import "./Header.css";
 
 const Header = () => (
-  <div>
-    Header
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+      <SubHeading title="Chase the new flavaur" />
+      <h1 className="app__header-h1">The key to Fine Dinning</h1>
+      <p className="p__opensans" style={{ margin: "2rem 0" }}>
+        Sit Tellus Lobortis Sed Senectus Vivamus Molestie. Condimentum Volutpat
+        Morbi Facilisis Quam Scelerisque Sapien. Et, Penatibus Aliquam Amet
+        Tellus
+      </p>
+      <button className="custom__button">Explore Menu</button>
+    </div>
+
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="welcome" />
+    </div>
   </div>
 );
 
