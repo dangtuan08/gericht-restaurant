aa2a548bbd6cb1fdea4a0249e5bca5c9250c2594
Tuan <tuanou17@gmail.com>
2022-07-05 16:25:20 +0700

    Special Menu

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from "react";
 
-import './MenuItem.css';
+import "./MenuItem.css";
 
-const MenuItem = () => (
-  <div>
-    MenuItem
-  </div>
-);
+const MenuItem = ({ item }) => {
+  console.log(item);
+  return (
+    <div className="app__menuitem">
+      <div className="app__menuitem-head">
+        <div className="app__menuitem-head_name">
+          <p className="p__cormorant">{item.title}</p>
+        </div>
+        <div className="app__menuitem-dash"></div>
+        <div className="app__menuitem-price">
+          <p className="p__cormorant">{item.price}</p>
+        </div>
+      </div>
+      <div className="app__menuitem-sub">
+        <p className="p__opensans">{item.tags}</p>
+      </div>
+    </div>
+  );
+};
 
 export default MenuItem;
